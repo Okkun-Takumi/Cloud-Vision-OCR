9e6460cb9ac5bc4d71f5cea40e356d8ac4e4e68a
Okuyama <1093saku3838@gmail.com>
2026-02-23 16:03:07 +0900

    fix upload image

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import streamlit as st
 from PIL import Image
 
-file_path = st.file_uploader('',type=['png','jpg','jpeg'])
-img = Image.open(file_path)
-st.image(img)
+file = st.file_uploader('OCRしたい画像をアップロード',type=['png','jpg','jpeg'])
+
+if file:
+    img = Image.open(file)
+    st.image(
+        img,
+        caption="アップロードした画像",
+        use_container_width = True
+        )
